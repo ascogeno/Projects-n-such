73be66fe4822be8675bd363ec0c46867eb66a679
ascogeno <rosengren.kyle@gmail.com>
2026-01-15 22:03:08 -0700

    did some tests

--- Sprint1/sprint.cpp
+++ Sprint1/sprint.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
 using namespace std;
 
+void conditionalTest(int x)
+{
+    if (x == 0)
+    {
+        cout << "That was nothing";
+    }
+
+    else if (x >= 1 && x < 100)
+    {
+        cout << "That was something";
+    }
+
+    else if (x < 0)
+    {
+        cout << "Hey man, don't do that. Stay positive";
+    }
+
+    else
+    {
+        cout << "Whoa, that's a lot";
+    }
+}
+
 int main()
 {
-    cout << "Hello World!";
+    int x;
+    cout << "Hello World! \n";
+    cout << "Enter a number please:";
+    cin >> x;
+    cout << "\nYour number was " << x << "\n";
+    conditionalTest(x);
+
     return 0;
 }
